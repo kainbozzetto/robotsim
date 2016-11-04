a159d91566a4f168875c14c11e0cac03da9d6281
unknown <kainbozzetto@gmail.com>
2016-11-04 12:29:35 +1100

    Updated Tabletop class with isValidPosition method

--- app/Tabletop.js
+++ app/Tabletop.js
@@ -1,7 +1,18 @@
 'use strict';
 
 class Tabletop {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 
+  isValidPosition(x, y) {
+    if (x < 0 || y < 0 || x >= this.width || y >= this.height) {
+      return false;
+    }
+    
+    return true;
+  }
 }
 
 module.exports = Tabletop;
