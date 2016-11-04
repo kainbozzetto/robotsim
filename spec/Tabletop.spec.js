a2935d35a1e401be94517e3652582df799e3974b
unknown <kainbozzetto@gmail.com>
2016-11-04 12:24:37 +1100

    Updated Tabletop testing spec

--- spec/Tabletop.spec.js
+++ spec/Tabletop.spec.js
@@ -1,5 +1,33 @@
+var Tabletop = require('../app/Tabletop');
+
 describe('Tabletop Class', function() {
-  it('should return true', function() {
-    expect(true).toBe(true);
+
+  var tabletop = new Tabletop(5, 5);
+
+  it('should be defined', function() {
+     expect(tabletop).toBeDefined();
   });
+
+  describe('isValidPosition method', function() {
+
+    it('should be an invalid position', function() {
+      var validation = tabletop.isValidPosition(-1, 0);
+
+      expect(validation).toBe(false);
+    });
+
+    it('should be another invalid position', function() {
+      var validation = tabletop.isValidPosition(2, 6);
+
+      expect(validation).toBe(false);
+    });
+
+    it('should be a valid position', function() {
+      var validation = tabletop.isValidPosition(2, 3);
+
+      expect(validation).toBe(true);
+    });
+
+  });
+
 });
